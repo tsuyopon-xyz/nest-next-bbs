c958b0404824da0e19a6e162ea28cd8a5df29e31
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-08-12 13:47:01 +0900

    Setup TypeORM and create User entity to create User table

--- backend/app/src/app.module.ts
+++ backend/app/src/app.module.ts
@@ -1,13 +1,50 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { isDevelopment, isTest } from './utils/env';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'test', 'production')
+          .required(),
+        PORT: Joi.number().required(),
+      }),
+      envFilePath: `${process.cwd()}/.env`,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: isTest()
+        ? process.env.DATABASE_USER_TEST
+        : process.env.DATABASE_USER,
+      password: isTest()
+        ? process.env.DATABASE_PASSWORD_TEST
+        : process.env.DATABASE_PASSWORD,
+      database: isTest()
+        ? process.env.DATABASE_NAME_TEST
+        : process.env.DATABASE_NAME,
+      entities: [User],
+      synchronize: isDevelopment() || isTest(),
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController, AuthController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env);
+  }
+}
